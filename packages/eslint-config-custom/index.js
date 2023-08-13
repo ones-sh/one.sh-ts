edeef3ebd0c7ab027e4d40c9ec07a86b12475d35
Ronit Panda <ronit@superkit.io>
2023-08-13 10:05:47 +0530

    feat: initial commit sets up repository

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,27 @@
+/** @type {import("eslint").Linter.Config} */
+
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+    root: true,
+    extends: ['next', 'plugin:prettier/recommended', 'turbo'],
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'],
     },
-  },
+    settings: {
+        next: {
+            rootDir: ['apps/*/', 'packages/*/'],
+        },
+    },
+    rules: {
+        '@next/next/no-img-element': 'off',
+        '@next/next/no-html-link-for-pages': 'off',
+        'jsx-a11y/role-supports-aria-props': 'off', // @see https://github.com/vercel/next.js/issues/27989#issuecomment-897638654
+        'react/jsx-curly-brace-presence': [
+            'error',
+            { props: 'never', children: 'never' },
+        ],
+        'react/self-closing-comp': ['error', { component: true, html: true }],
+        'no-console': 1,
+    },
 };
+
---
+++ packages/prettier/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    bracketSameLine: true,
+    singleQuote: false,
+    arrowParens: 'always',
+    tabWidth: 4,
+};
+
