03f74db2d3da026347880b84e22a101092226f1b
Ronit Panda <ronit@superkit.io>
2023-08-13 10:06:17 +0530

    style: cleanup

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,26 +2,25 @@
 
 module.exports = {
     root: true,
-    extends: ['next', 'plugin:prettier/recommended', 'turbo'],
+    extends: ["next", "plugin:prettier/recommended", "turbo"],
     parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'],
+        project: ["./apps/*/tsconfig.json", "./packages/*/tsconfig.json"],
     },
     settings: {
         next: {
-            rootDir: ['apps/*/', 'packages/*/'],
+            rootDir: ["apps/*/", "packages/*/"],
         },
     },
     rules: {
-        '@next/next/no-img-element': 'off',
-        '@next/next/no-html-link-for-pages': 'off',
-        'jsx-a11y/role-supports-aria-props': 'off', // @see https://github.com/vercel/next.js/issues/27989#issuecomment-897638654
-        'react/jsx-curly-brace-presence': [
-            'error',
-            { props: 'never', children: 'never' },
+        "@next/next/no-img-element": "off",
+        "@next/next/no-html-link-for-pages": "off",
+        "jsx-a11y/role-supports-aria-props": "off", // @see https://github.com/vercel/next.js/issues/27989#issuecomment-897638654
+        "react/jsx-curly-brace-presence": [
+            "error",
+            { props: "never", children: "never" },
         ],
-        'react/self-closing-comp': ['error', { component: true, html: true }],
-        'no-console': 1,
+        "react/self-closing-comp": ["error", { component: true, html: true }],
+        "no-console": 1,
     },
 };
-
